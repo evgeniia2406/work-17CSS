91211c0045af263bd50e6ee9c2768756b80e7089
Евгения <68277705+evgeniia2406@users.noreply.github.com>
2021-04-29 22:52:49 +0300

    added js file

---
+++ java.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName('header__burger')[0].addEventListener('click', function() {
+        document.getElementsByClassName('header__burger')[0].classList.toggle('active');
+                document.getElementsByClassName('header__menu')[0].classList.toggle('active');
+                document.getElementsByTagName('body')[0].classList.toggle('lock');
+})
